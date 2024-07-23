6662ce57b419216d9c86b4a331f8d8c51c2cfee9
Rabin Gurung <wrabeeng@gmail.com>
2024-07-23 14:22:33 +0545

    feat: react oauth react install

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { GoogleLogin } from "@react-oauth/google";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <GoogleLogin
+        onSuccess={(credential) => console.log(credential)}
+        onError={() => console.log("Sign in failed!")}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
